5fea1ae1b60d4c2cfc01aef58658f480270a2aab
cpatarroyo <ca.patarroyo960@hotmail.com>
2023-06-01 12:26:16 +0200

    Added the example data and created the documentation that goes along

---
+++ R/Data_Description.R
@@ -0,0 +1,35 @@
+#' Microsatellite genotypes of the Colombian population
+#'
+#' A set of samples of *P. infestans* collected in Cundinamarca and characterized by 12 microsatellite loci: D13, G11, Pi02, Pi04, Pi4B, Pi63, Pi70, SSR11, SSR2, SSR4, SSR6, and SSR8.
+#'
+#' @format Object of the clas Genclone as defined by the adegenet R package.
+#' @source https://doi.org/10.1094/PHYTO-05-18-0157-R
+#'
+"Pinf_Colombia"
+
+#' Microsatellite genotypes of the Colombian population
+#'
+#' A set of samples of *P. infestans* collected in Mexico and characterized by 12 microsatellite loci: D13, G11, Pi02, Pi04, Pi4B, Pi63, Pi70, SSR11, SSR2, SSR4, SSR6, and SSR8.
+#'
+#' @format Object of the clas Genclone as defined by the adegenet R package.
+#' @source https://doi.org/10.1111/mec.14000
+#'
+"Pinf_Mexico"
+
+#' Microsatellite genotypes of the Colombian population
+#'
+#' A set of samples of *P. infestans* collected in Korea and characterized by 12 microsatellite loci: D13, G11, Pi02, Pi04, Pi4B, Pi63, Pi70, SSR11, SSR2, SSR4, SSR6, and SSR8.
+#'
+#' @format Object of the clas Genclone as defined by the adegenet R package.
+#' @source https://bsppjournals.onlinelibrary.wiley.com/doi/10.1111/ppa.13178
+#'
+"Pinf_Korea"
+
+#' Microsatellite genotypes of the Colombian population
+#'
+#' A set of samples of *P. infestans* collected in Korea and characterized by 8 microsatellite loci: Pi4B Pi02 G11 Pi33 Pi56 Pi16 Pi70 Pi63
+#'
+#' @format Object of the clas Genclone as defined by the adegenet R package.
+#' @source https://onlinelibrary.wiley.com/doi/10.1111/jeu.12010
+#'
+"Pinf_China"
